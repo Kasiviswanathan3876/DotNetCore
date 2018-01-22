5fa697e255f7d85fec00fa478165983051ac5bf3
No.8 <zhaobingwang@foxmail.com>
2018-01-22 22:51:50 +0800

    JWT-authentication，token

---
+++ Demo/JwtAuthSample/Controllers/AuthorizeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JwtAuthSample.Models;
+using JwtAuthSample.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtAuthSample.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class AuthorizeController : Controller
+    {
+        private JwtSettings _jwtSettings;
+        public AuthorizeController(IOptions<JwtSettings> _jwtSettingsAccesser)
+        {
+            _jwtSettings = _jwtSettingsAccesser.Value;
+        }
+
+        [HttpPost]
+        [Route("token")]
+        public IActionResult Token([FromBody]LoginViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!(viewModel.User == "no8" && viewModel.Password == "123456"))
+                    return BadRequest();
+                var claims = new Claim[] {
+                    new Claim(ClaimTypes.Name,"no8"),
+                    new Claim(ClaimTypes.Role,"admin")
+                };
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var token = new JwtSecurityToken(_jwtSettings.Issuer, _jwtSettings.Audience, claims, DateTime.Now, DateTime.Now.AddMinutes(30), creds);
+                return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+            }
+            return BadRequest();
+        }
+    }
+}
